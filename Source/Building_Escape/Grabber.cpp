589f70db485708ade68437ef2d1ffda292f982b6
Martin Fučík <mar.fucik@gmail.com>
2021-10-11 15:31:48 +0200

    Accessors & Memory Layout

--- Source/Building_Escape/Grabber.cpp
+++ Source/Building_Escape/Grabber.cpp
@@ -30,12 +30,18 @@ void UGrabber::BeginPlay()
     if (InputComponent)
     {
         InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+        InputComponent->BindAction("Grab", IE_Released, this, &UGrabber::Release);
     }
 }
 
 void UGrabber::Grab()
 {
-    UE_LOG(LogTemp, Warning, TEXT("Grab presed!"));
+    UE_LOG(LogTemp, Warning, TEXT("Grab pressed!"));
+}
+
+void UGrabber::Release()
+{
+    UE_LOG(LogTemp, Warning, TEXT("Grab released!"));
 }
 
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
